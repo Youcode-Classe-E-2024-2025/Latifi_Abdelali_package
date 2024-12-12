ea664cb3ebf73bd55948cd564b9f0b1941773333
latifi777 <abdelalilatifi3@gmail.com>
2024-12-12 10:44:33 +0100

    en travaillant sur la partie de suppresion

--- delete.php
+++ delete.php
@@ -1,5 +1,10 @@
 <?php
 require 'connexion.php' ;
 $id = $_GET['id'];
+$sql = "delete from allpackages where id = '$id'";
+$query = mysqli_query($con,$sql);
+if(isset($query)){
+    echo 'hhhhhhhhhh';
+}
 
 ?>
