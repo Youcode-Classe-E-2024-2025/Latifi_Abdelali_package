0ec9e65cb9362c90315946a5138588328742f1e4
latifi777 <abdelalilatifi3@gmail.com>
2024-12-12 11:08:54 +0100

    le fonctionment de suppression est validé

--- delete.php
+++ delete.php
@@ -4,7 +4,7 @@ $id = $_GET['id'];
 $sql = "delete from allpackages where id = '$id'";
 $query = mysqli_query($con,$sql);
 if(isset($query)){
-    echo 'hhhhhhhhhh';
+header("location:aff_allpckg.php");
 }
 
 ?>
